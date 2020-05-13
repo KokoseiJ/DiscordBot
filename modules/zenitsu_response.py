fcd730c4e4c18a218030cf47ee860a8a6c2440be
KokoseiJ <kokoseij@gmail.com>
2020-05-14 04:52:15 +0900

    Fixed zenitsu_response module as zenitsu is now dead

--- modules/zenitsu_response.py
+++ modules/zenitsu_response.py
@@ -3,5 +3,5 @@ PERMISSION = 6
 HELP = '"하라는 코딩은 안하고!" 라는 문장에 반응합니다.'
 
 async def main(message):
-    if "하라는 코딩은 안하고" in message.content:
-        return "ㄹㅇ;;"
+    if ";;" in message.content:
+        return "하라는 코딩은 안하고!"
